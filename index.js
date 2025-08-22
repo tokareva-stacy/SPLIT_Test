996cb5491dbad0f1c57c2d5e475492b5f08a8a3d
tokareva-stacy <tokarevanyura@gmail.com>
2025-08-22 14:11:14 +0000

    Deploying to gh-pages from @ tokareva-stacy/SPLIT_Test@b48a7d371438399cddb0aad5b6f2eca71f9371a4 🚀

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&n(c)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();(()=>{const s=document.querySelector("[data-menu]"),o=document.querySelector("[data-menu-open]"),r=document.querySelector("[data-menu-close]"),n=document.body;if(s&&o&&r){const e=()=>{s.classList.toggle("is-open"),n.classList.toggle("is-menu-open")};o.addEventListener("click",e),r.addEventListener("click",e)}})();
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&r(c)}).observe(document,{childList:!0,subtree:!0});function o(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function r(e){if(e.ep)return;e.ep=!0;const s=o(e);fetch(e.href,s)}})();(()=>{const n=document.querySelector("[data-menu]"),t=document.querySelector("[data-menu-open]"),o=document.querySelector("[data-menu-close]"),r=document.body;if(n&&t&&o){const e=()=>{n.classList.toggle("is-open"),r.classList.toggle("is-menu-open")};t.addEventListener("click",e),o.addEventListener("click",e)}})();document.addEventListener("DOMContentLoaded",()=>{const n=document.getElementById("products");fetch("../json/products.json").then(t=>{if(!t.ok)throw new Error("Network error: "+t.statusText);return t.json()}).then(t=>{t.forEach(o=>{const r=document.createElement("div");r.classList.add("product-card");let e="";o.isSellingFast?e='<div class="status-tag selling-fast">SELLING FAST</div>':o.isSoldOut&&(e='<div class="status-tag sold-out">SOLD OUT</div>');let s="",c="",i="";o.oldPrice&&(s=`<span class="old-price">$${o.oldPrice}</span>`,c="has-old-price",i='<div class="discount-tag">20% OFF</div>'),r.innerHTML=`
+                    <div class="product-image-wrapper">
+                        <img src="${o.imageUrl}" alt="${o.name}">
+                        ${e}
+                        ${i}
+                    </div>
+                    <h3 class="product-name">${o.name}</h3>
+                    <div class="product-price">
+                        <span class="current-price ${c}">$${o.price}</span>
+                        ${s}
+                    </div>
+                    <button class="add-to-cart-btn">
+                        <svg width="16" height="24" class="add-to-cart-icon">
+                            <use href="./img/symbol-defs.svg#icon-cart"></use>
+                        </svg>
+                    </button>
+                `,n.appendChild(r)})}).catch(t=>console.error("Error while receiving goods:",t))});const l=document.querySelector(".view-all-link");l&&l.addEventListener("click",n=>{n.preventDefault(),productsContainer.scrollIntoView({behavior:"smooth",block:"start"})});
 //# sourceMappingURL=index.js.map
