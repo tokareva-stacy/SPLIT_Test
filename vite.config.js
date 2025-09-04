936a7e2c2142e7f0f2360139cbb82a103d984590
tokareva-stacy <tokarevanyura@gmail.com>
2025-09-04 17:11:18 +0300

    base: '/SPLIT_Test/'

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/SPLIT_Test/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
